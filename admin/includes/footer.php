be1a622da9deb849200bb027859ab63c576a2dde
Eys07 <cjacedelfin10@gmail.com>
2026-01-19 09:39:33 +0800

    tel number

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 mb-2"> 
-                <small><i class="fas fa-phone me-2"></i> <a href="tel:+63781234567" class="text-white text-decoration-none">(078) 123-4567</a></small> |
+                <small><i class="fas fa-phone me-2"></i> <a href="tel:+63781234567" class="text-white text-decoration-none">09161691871</a></small> |
                 <small><i class="fas fa-envelope me-2"></i> <a href="mailto:poblacionsouth.solano@gmail.com" class="text-white text-decoration-none">poblacionsouth.solano@gmail.com</a></small> <br>
                 <small><i class="fab fa-facebook-f me-2"></i><a href="https://www.facebook.com/profile.php?id=61574579927079" class="text-white text-decoration-none" target="_blank">Barangay Poblacion South, Solano</a></small>
             </div>
